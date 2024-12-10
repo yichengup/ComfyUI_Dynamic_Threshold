54945c4aef12c1ad66ee346c2bac8a18fe84047a
yicheng <160642431+yichengup@users.noreply.github.com>
2024-12-10 12:07:18 +0000

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,13 @@
+﻿from .DynamicThreshold import DynamicThreshold
+
+NODE_CLASS_MAPPINGS = {
+    # ... other nodes
+    "DynamicThreshold": DynamicThreshold
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    # ... other nodes
+    "DynamicThreshold": "Dynamic Threshold" 
+}
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS'] 
